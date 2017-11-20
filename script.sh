1e9ebff293af7d9256c31b2e1c1237edb1880059
Edwin Montoya Jaramillo <emonto15@eafit.edu.co>
2017-11-20 16:26:51 -0500

    Commenting all the output for testing

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 
 for i in $(seq 4 10);
     do
-	spark-submit clustering.py /datasets/gutenberg-txt-es/ $i 2000 spanish /user/emonto15/out$1
+	start=`date +%s`
+	spark-submit clustering.py /datasets/gutenberg-txt-es/ $i 2000 spanish /user/emonto15/out$i >  /dev/null 2>&1
+  end=`date +%s`
+  runtime=$((end-start))
+  echo "Time with K=$i was $runtime"
     done
