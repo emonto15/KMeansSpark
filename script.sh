858367dac153a9d2fba33f7c021d4942e62e2697
Edwin Montoya Jaramillo <emonto15@eafit.edu.co>
2017-11-21 12:11:38 -0500

    Adding the dataset and the tecnical report

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 for i in $(seq 4 10);
     do
 	start=`date +%s`
-	spark-submit clustering.py /datasets/gutenberg-txt-es/ $i 2000 spanish /user/emonto15/out$i >  /dev/null 2>&1
+	a=$(spark-submit --master yarn --deploy-mode cluster  --executor-memory 2G --num-executors clustering.py /datasets/gutenberg-txt-es/ $i 2000 spanish /user/emonto15/out$i)
   end=`date +%s`
   runtime=$((end-start))
   echo "Time with K=$i was $runtime"
